cf6f266291490d928c5b71d2928cb9e5caed5cb5
Sarika <sarika@ngoyal.com>
2019-03-24 21:17:52 -0700

    Enhancements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,119 +1,78 @@
-
-	
-			 let toggle = true
-
-			 let itemsArray = ["Gucci", "Prada", "Versace", "Louis Vuitton", "Hermes", "Chanel", "Rolex"]
+let toggle = true
+let itemsArray = ["Gucci", "Prada", "Versace", "Louis Vuitton", "Hermes", "Chanel", "Rolex"]
 
 
-			 function renderbuttons()
-			 	 {
-
-					document.querySelector('#btnsList').innerHTML = ""
-					itemsArray.forEach(item =>{
-				let btnElm = document.createElement('button')
+function renderbuttons() {
+    document.querySelector('#btnsList').innerHTML = ""
+    itemsArray.forEach(item => {
+        let btnElm = document.createElement('button')
         btnElm.innerHTML = item
         btnElm.className = 'btn shopping_btn'
         btnElm.setAttribute('brand', item)
         document.querySelector('#btnsList').append(btnElm)
-			document.querySelector('#btnsList').append(document.createElement('br'))
-   		 })
+        document.querySelector('#btnsList').append(document.createElement('br'))
+    })
+}
 
-			}
-			 function displayGifs(word,number)
-			 {
+function displayGifs(word, number) {
+    document.querySelector('#gif_container').innerHTML = ""
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${word}&api_key=V3b70ll8C9vGwtzZvbZAfCdMmTwdwX40&rating=g&limit=10`)
+        .then(r => r.json())
+        .then(r => {
+            r.data.forEach(gif => {
 
-			   document.querySelector('#gif_container').innerHTML = ""
+                let still = gif.images.fixed_height_still.url
+                let rating = gif.rating.toUpperCase();
+                let { url: animated
+                } = gif.images.fixed_height
 
-			   fetch(`https://api.giphy.com/v1/gifs/search?q=${word}&api_key=V3b70ll8C9vGwtzZvbZAfCdMmTwdwX40&rating=g&limit=10`)
-			   .then(r => r.json())
-			   .then(r => {
-			     r.data.forEach(gif => {
+                let ratingElement = document.createElement('span')
+                ratingElement.className = 'rating_text'
+                let gifElem = document.createElement('img')
+                gifElem.className = "gif_img"
+                gifElem.setAttribute('src', still)
+                gifElem.setAttribute('data-still', still)
+                gifElem.setAttribute('data-animated', animated)
+                gifElem.setAttribute('alt', word)                
+                ratingElement.innerHTML = `Rating: ${rating}<br>`
+                ratingElement.appendChild(gifElem)
+                document.querySelector('#gif_container').append(ratingElement)
+            })
 
-			      // console.log(gif)
-					 let still = gif.images.fixed_height_still.url
+        })
+        .catch(e => console.error(e))
+}
 
-					 let rating = gif.rating.toUpperCase();
+    document.addEventListener('click', (event) => {
+    if (event.target.className === 'btn shopping_btn') {
+        console.log("Inside Event Listener")
+        let brand = event.target.getAttribute('brand')
+        displayGifs(brand, 10)
+    } else if (event.target.className === 'gif_img') {
+        toggle = !toggle
+        let {
+            still,
+            animated
+        } = event.target.dataset
+        console.log("Toggle=>" + toggle + "Still=> " + still + "animated=>" + animated)
+        if (toggle) {
+            event.target.setAttribute('src', animated)
+            console.log("inside toggle true=> " + toggle + "Still=> " + still + "animated=>" + animated)
+        } else {
+            event.target.setAttribute('src', still)
+            console.log("Toggle false=> " + toggle + "Still=> " + still + "animated=>" + animated)
+        }
+    }
 
-					// console.log(r)					 					 
-					// console.log( "Still?=?" + still + gif.images.fixed_width_still)
-					 let { url: animated } = gif.images.fixed_height
-					 
-					 //console.log( "animate?=?" + animated + gif.images.fixed_width)
+    if (event.target.className === 'btn btn-warning') {
+        event.preventDefault()
+        console.log("Submit Btn Pressed")
+        let newItem = document.querySelector('#moreItems').value
+        displayGifs(newItem, 10)
+        itemsArray.push(newItem)
+        renderbuttons()
+    }
+  })
 
-						let ratingElement = document.createElement('span')
-						
-						ratingElement.className = 'rating_text'
-						
-
-					
-			       let gifElem = document.createElement('img')
-						 gifElem.className = "gif_img"
-						 gifElem.setAttribute('src', still)
-						 gifElem.setAttribute('data-still', still)
-			       gifElem.setAttribute('data-animated', animated)
-			       gifElem.setAttribute('alt', word)
-			       //document.querySelector('#gif_container').append(gifElem)
-
-						 ratingElement.innerHTML = `Rating: ${rating}<br>`
-						 //ratingElement.innerHTML = `Rating: ${rating}<br><img src=${still} alt="Test">`
-
-						 ratingElement.appendChild(gifElem)
-
-						 document.querySelector('#gif_container').append(ratingElement)
-
-						console.log(ratingElement.innerHTML)
-						 //document.querySelector('#gif_container').append(gifElem)
-
-					
-						
-
-
-			     })
-
-			      })
-				 .catch(e => console.error(e))
-			 }
-
-			document.addEventListener('click', (event) => {
-
-//				.catch(e => console.error(e))       
-
-			console.log("Inside Event Listener")
-			if (event.target.className === 'btn shopping_btn') {
-					 console.log("Inside Event Listener")
-			   let brand = event.target.getAttribute('brand')	 
-			displayGifs(brand,10)
-			   
-			    } else if (event.target.className === 'gif_img') {
-			     toggle = !toggle
-				let { still, animated } = event.target.dataset
-				console.log("Toggle=>" + toggle + "Still=> " + still + "animated=>" + animated )
-			    if (toggle) {
-					event.target.setAttribute('src', animated)
-					console.log( "inside toggle true=> " + toggle + "Still=> " + still + "animated=>" + animated )
-		      } else {
-						event.target.setAttribute('src', still)
-						console.log("Toggle false=> " + toggle + "Still=> " + still + "animated=>" + animated )
-					}
-			   }
-						
-		            if (event.target.className === 'btn btn-primary') {
-
-                event.preventDefault()
-
-                console.log("Submit Btn Pressed")
-
-								let newItem = document.querySelector('#moreItems').value
-								displayGifs(newItem, 10)
-								
-								itemsArray.push(newItem)
-								renderbuttons()
-
-             }
-
-
-			})
-
-			renderbuttons()
-			displayGifs(itemsArray[0],10)
-			
+renderbuttons()
+displayGifs(itemsArray[0], 10)
